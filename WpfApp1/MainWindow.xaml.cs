41bf3ea81837e1608bffaf819ea85bfe84408c24
caoxin <caoxin_89@outlook.com>
2019-08-22 11:02:28 +0800

    add 1102

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.IO;
 namespace WpfApp1
 {
     /// <summary>
@@ -24,5 +24,114 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_OP01(object sender, RoutedEventArgs e)
+        {
+ 
+            string path1 = @"D:\test\OP01\HmiRTm.ini";
+            string path2 = @"C:\Program Files (x86)\Siemens\Automation\WinCC RT Advanced\HmiRTm.ini";
+            FileInfo fi1 = new FileInfo(path1);
+            FileInfo fi2 = new FileInfo(path2);
+
+            try
+            {
+                // Create the source file.
+                // using (FileStream fs = fi1.Create()) { }
+
+                //Ensure that the target file does not exist.
+                if (File.Exists(path2))
+                {
+                    fi2.Delete();
+                }
+
+                //Copy the file.f
+                // fi1.CopyTo(path2);
+                File.Copy(path1, path2, true);
+                Console.WriteLine("{0} was copied to {1}.", path1, path2);
+            }
+            catch (IOException ioex)
+            {
+                Console.WriteLine(ioex.Message);
+            }
+
+
+
+            string path3 = @"C:\Program Files (x86)\Siemens\Automation\WinCC RT Advanced\HmiRTM.exe";
+            System.Diagnostics.Process.Start(path3);
+
+        }
+
+        private void Button_入库端(object sender, RoutedEventArgs e)
+        {
+            string path1 = @"D:\test\OP02\HmiRTm.ini";
+            string path2 = @"C:\Program Files (x86)\Siemens\Automation\WinCC RT Advanced\HmiRTm.ini";
+            FileInfo fi1 = new FileInfo(path1);
+            FileInfo fi2 = new FileInfo(path2);
+
+            try
+            {
+                // Create the source file.
+               // using (FileStream fs = fi1.Create()) { }
+
+                //Ensure that the target file does not exist.
+                if (File.Exists(path2))
+                {
+                    fi2.Delete();
+                }
+
+                //Copy the file.f
+               // fi1.CopyTo(path2);
+                File.Copy(path1, path2, true);
+                Console.WriteLine("{0} was copied to {1}.", path1, path2);
+            }
+            catch (IOException ioex)
+            {
+                Console.WriteLine(ioex.Message);
+            }
+       
+            string path3 = @"C:\Program Files (x86)\Siemens\Automation\WinCC RT Advanced\HmiRTM.exe";
+            System.Diagnostics.Process.Start(path3);
+        }
+
+        private void Button_出库端(object sender, RoutedEventArgs e)
+        {
+            string path1 = @"D:\test\OP03\HmiRTm.ini";
+            string path2 = @"C:\Program Files (x86)\Siemens\Automation\WinCC RT Advanced\HmiRTm.ini";
+            FileInfo fi1 = new FileInfo(path1);
+            FileInfo fi2 = new FileInfo(path2);
+
+            try
+            {
+                // Create the source file.
+                // using (FileStream fs = fi1.Create()) { }
+
+                //Ensure that the target file does not exist.
+                if (File.Exists(path2))
+                {
+                    fi2.Delete();
+                }
+
+                //Copy the file.f
+                // fi1.CopyTo(path2);
+                File.Copy(path1, path2, true);
+              
+            }
+            catch (IOException ioex)
+            {
+                Console.WriteLine(ioex.Message);
+            }
+
+
+
+            string path3 = @"C:\Program Files (x86)\Siemens\Automation\WinCC RT Advanced\HmiRTM.exe";
+            System.Diagnostics.Process.Start(path3);
+        }
+
+       
     }
 }
