fabd6f257a82bfac0b0e3482b6b0da2c38df1783
caoxin <caoxin_89@outlook.com>
2020-04-29 10:24:46 +0800

    增加类，可对ini文件进行操作，通过配置文件更改位置

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace WpfApp1
         private string Disk1 = ConfigurationManager.AppSettings["终端1组态文件位置"].ToString();
         private string Disk2 = ConfigurationManager.AppSettings["终端2组态文件位置"].ToString();
         private string Disk3 = ConfigurationManager.AppSettings["终端3组态文件位置"].ToString();
-      
+        private string Disk4 = ConfigurationManager.AppSettings["终端4组态文件位置"].ToString();
         public MainWindow()
         {
             InitializeComponent();
@@ -57,7 +57,68 @@ namespace WpfApp1
 
         private void Button_画面一(object sender, RoutedEventArgs e)
         {
+            string path4 = @"HmiRTm";
+            closeProc(path4);
+            System.Threading.Thread.Sleep(1000);
+            if (!File.Exists(pathIni))
+            {
+                using (File.Create(pathIni)) { };
+            }
 
+            Enfon画面选择.IniFileHelper.SetValue("configuration", "LoadConfigfile", Disk1, pathIni);
+            string path3 = pathExe;
+            System.Diagnostics.Process.Start(path3);
+
+
+        }
+
+        private void Button_画面二(object sender, RoutedEventArgs e)
+        {
+            string path4 = @"HmiRTm";
+            closeProc(path4);
+            System.Threading.Thread.Sleep(1000);
+            if (!File.Exists(pathIni))
+            {
+                using (File.Create(pathIni)) { };
+            }
+
+            Enfon画面选择.IniFileHelper.SetValue("configuration", "LoadConfigfile", Disk2, pathIni);
+            string path3 = pathExe;
+            System.Diagnostics.Process.Start(path3);
+        }
+
+        private void Button_画面三(object sender, RoutedEventArgs e)
+        {
+            string path4 = @"HmiRTm";
+            closeProc(path4);
+            System.Threading.Thread.Sleep(1000);
+            if (!File.Exists(pathIni))
+            {
+                using (File.Create(pathIni)) { };
+            }
+
+            Enfon画面选择.IniFileHelper.SetValue("configuration", "LoadConfigfile", Disk3, pathIni);
+            string path3 = pathExe;
+            System.Diagnostics.Process.Start(path3);
+        }
+        private string local;
+        private void readIniFile()
+        {
+            if (File.Exists(pathIni))
+            {
+                local= Enfon画面选择.IniFileHelper.GetValue("configuration", "LoadConfigfile", pathIni);
+            }
+            else
+            {
+                MessageBox.Show("文件加载失败，请确认是否存在此文件：" + pathIni);
+            }
+        }
+
+
+      
+
+     /*   private void Button_Click(object sender, RoutedEventArgs e)  //文件复制备份
+        {
             string path4 = @"HmiRTm";  //需要关闭进程的名字，如果不知道进程的名称，可以通过closeProc里面的list去查找
             closeProc(path4);  //关闭进程
             System.Threading.Thread.Sleep(1000);
@@ -93,99 +154,6 @@ namespace WpfApp1
             string path3 = pathExe;
             System.Diagnostics.Process.Start(path3);
 
-        }
-
-        private void Button_画面二(object sender, RoutedEventArgs e)
-        {
-            string path4 = @"HmiRTm";
-            closeProc(path4);
-            System.Threading.Thread.Sleep(1000);
-            string path1 = Disk2;
-            string path2 = pathIni;
-            FileInfo fi1 = new FileInfo(path1);
-            FileInfo fi2 = new FileInfo(path2);
-
-            try
-            {
-                // Create the source file.
-               // using (FileStream fs = fi1.Create()) { }
-
-                //Ensure that the target file does not exist.
-                if (File.Exists(path2))
-                {
-                    fi2.Delete();
-                }
-
-                //Copy the file.f
-               // fi1.CopyTo(path2);
-                File.Copy(path1, path2, true);
-                Console.WriteLine("{0} was copied to {1}.", path1, path2);
-            }
-            catch (IOException ioex)
-            {
-                Console.WriteLine(ioex.Message);
-            }
-       
-            string path3 = pathExe;
-            System.Diagnostics.Process.Start(path3);
-        }
-
-        private void Button_画面三(object sender, RoutedEventArgs e)
-        {
-            string path4 = @"HmiRTm";
-            closeProc(path4);
-            System.Threading.Thread.Sleep(1000);
-            string path1 = Disk3;
-            string path2 = pathIni;
-            FileInfo fi1 = new FileInfo(path1);
-            FileInfo fi2 = new FileInfo(path2);
-
-            try
-            {
-                // Create the source file.
-                // using (FileStream fs = fi1.Create()) { }
-
-                //Ensure that the target file does not exist.
-                if (File.Exists(path2))
-                {
-                    fi2.Delete();
-                }
-
-                //Copy the file.f
-                // fi1.CopyTo(path2);
-                File.Copy(path1, path2, true);
-              
-            }
-            catch (IOException ioex)
-            {
-                Console.WriteLine(ioex.Message);
-            }
-
-
-
-            string path3 = pathExe;
-            System.Diagnostics.Process.Start(path3);
-        }
-        private string local;
-        private void readIniFile()
-        {
-            if (File.Exists(pathIni))
-            {
-                local= Enfon画面选择.IniFileHelper.GetValue("configuration", "LoadConfigfile", pathIni);
-            
-                MessageBox.Show("local");
-                MessageBox.Show("读取文档成功");
-            }
-            else
-            {
-                MessageBox.Show("文件加载失败，请确认是否存在此文件：" + pathIni);
-            }
-        }
-
-
-
-
-
-
+        } */
     }
 }
